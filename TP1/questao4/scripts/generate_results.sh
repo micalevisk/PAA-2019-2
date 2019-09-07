6c9fc1fa80b3d7ec930911589b664249f545f1d1
Micael Levi L. Cavalcante <mllc@icomp.ufam.edu.br>
2019-09-07 14:43:23 -0400

    fix scripts (except radix)

--- TP1/questao4/scripts/generate_results.sh
+++ TP1/questao4/scripts/generate_results.sh
@@ -14,10 +14,26 @@ mkdir -p "$RESULTS_DIR"
 make MILLISECONDS=true
 make mostlyclean
 
-##
+clean_up() {
+  ARG=$?
+  if [ $ARG -eq 0 ]; then
+    echo "Finalizado!"
+  else
+    echo "Something went wrong!"
+    # read -n1 -p "Clean up? (n)"
+
+    rm -rf "$RESULTS_DIR"
+    rm -f "${CURR_INPUT_FILE}"
+    make clean
+    exit $ARG
+  fi
+}
+trap 'clean_up' EXIT ## Assume que nenhum comando pode dar erro (terminar com cod. diferente de 0)
+
+## ===============================================================
 CENARIOS=("A" "B") ## reconhecidos pelo `generate_input.py`
-INSTANCIAS=("20k" "40k" "60k" "80k" "100k" "120k" "140k" "160k")
-ALGORITMOS=("quicksort" "heapsort" "radixsort")
+INSTANCIAS=("20k" "40k" "60k" "80k" "100k" "120k")
+ALGORITMOS=("quicksort" "heapsort")
 QTD_VEZES=5 ## quantidade de execuções para a mesma entrada
 
 executar_cenario() {
@@ -31,6 +47,7 @@ executar_cenario() {
     instancia="${N//k/000}"
     ## Gerar entrada para a instância corrente
     arquivo_entrada="$(make -s generate SCENARIO=$cenario_id N=$instancia)"
+    CURR_INPUT_FILE="$arquivo_entrada"
 
     for algoritmo in "${ALGORITMOS[@]}"; do
       arquivo_saida="${cenario_dir}/${algoritmo}.exec_times-${instancia}.txt"
